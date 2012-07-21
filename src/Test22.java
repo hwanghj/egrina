789df365c991e7ce43c091c98ebb576cb9af6fb5
hwanghj <hwanghj@ais-info.co.jp>
2012-07-21 17:02:56 +0900

    first_hhj

--- src/Test22.java
+++ src/Test22.java
@@ -2,9 +2,11 @@
  * Created with IntelliJ IDEA.
  * User: Administrator
  * Date: 12/07/21
- * Time: 16:15
+ * Time: 16:59
  * To change this template use File | Settings | File Templates.
  */
-public class Test1 {
-
+public class Test22 {
+    private class tess{
+    }
+    }
 }
