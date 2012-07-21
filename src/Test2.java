5fcb107e94eae3b6b3e2aa0a9e3416eb2c3e58ee
Park Jiyeon <parkjy@ais-info.co.jp>
2012-07-21 16:56:48 +0900

    commented by jy

---
+++ src/Test2.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 12. 7. 21
+ * Time: 오후 4:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test2 {
+    HTTP
+}
