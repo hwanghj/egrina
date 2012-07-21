5df80d8605bc6a6acf14a7aa53fe76288a6149e3
hwanghj <hwanghj@ais-info.co.jp>
2012-07-21 16:16:38 +0900

    first_hhj

---
+++ src/Test1.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 12/07/21
+ * Time: 16:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test1 {
+}
